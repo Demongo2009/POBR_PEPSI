c1127c830121ae3ed0e8ea9a3bfba191da828bbe
Demongo2009 <kuba.galat1@wp.pl>
2021-11-16 18:02:35 +0100

    first

---
+++ cmake-build-debug/CMakeFiles/POBR_PEPSI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POBR_PEPSI.dir/main.cpp.obj"
+  "POBR_PEPSI.exe"
+  "POBR_PEPSI.exe.manifest"
+  "POBR_PEPSI.lib"
+  "POBR_PEPSI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POBR_PEPSI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
